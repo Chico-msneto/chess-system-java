9ce8bcf19f6c4d12f750b49d563ee24c2fc922f6
Francisco Neto <franciscomdsneto@gmail.com>
2024-01-31 11:32:39 -0300

    Chess layer and printboard implemented

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -4,17 +4,18 @@ public class Position {
 	
 	private int row;
 	private int column;
+	
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
 	
-	public int getRaw() {
+	public int getRow() {
 		return row;
 	}
 	
-	public void setRaw(int raw) {
-		this.row = raw;
+	public void setRow(int row) {
+		this.row = row;
 	}
 	
 	public int getColumn() {
