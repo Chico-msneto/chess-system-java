bc4a74cafa3c57395fcafc7b555b7700e5e6b31f
Francisco Neto <franciscomdsneto@gmail.com>
2024-01-31 10:53:49 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,32 @@
+package boardgame;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	public int getRaw() {
+		return row;
+	}
+	
+	public void setRaw(int raw) {
+		this.row = raw;
+	}
+	
+	public int getColumn() {
+		return column;
+	}
+	
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+}
